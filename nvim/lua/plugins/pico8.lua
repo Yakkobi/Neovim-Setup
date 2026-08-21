dfdcd7aeaa110d0db835176a5d70e8802f677f30
Yakkobi <289114684+Yakkobi@users.noreply.github.com>
2026-08-21 01:28:50 -0400

    Fix syntax warnings in .p8 files
    Previously, .p8 (pico8) files would throw
    syntax warnings with operators like -=,
    +=, and !=. Stopped this from happening
    by having lua_ls not attach in folders
    that contain .p8 files. pico8_ls will
    still attach to .p8 files however.

--- nvim/lua/plugins/pico8.lua
+++ nvim/lua/plugins/pico8.lua
@@ -1,25 +1,3 @@
-local pico8_globals = {
-  "cls", "spr", "sspr", "fget", "fset", "sget", "sset",
-  "mget", "mset", "map", "tline",
-  "pset", "pget", "color", "rect", "rectfill", "circ", "circfill",
-  "oval", "ovalfill", "line", "fillp",
-  "print", "cursor", "camera", "clip", "pal", "palt", "flip",
-  "btn", "btnp",
-  "sfx", "music",
-  "stat", "cartdata", "dget", "dset",
-  "memcpy", "memset", "reload", "cstore", "peek", "poke",
-  "peek2", "poke2", "peek4", "poke4",
-  "printh", "time", "t",
-  "rnd", "srand", "flr", "ceil", "abs", "sgn", "min", "max", "mid",
-  "sqrt", "sin", "cos", "atan2",
-  "band", "bor", "bxor", "bnot", "shl", "shr", "lshr", "rotl", "rotr",
-  "tostr", "tonum", "chr", "ord", "sub", "split",
-  "add", "del", "deli", "all", "foreach", "count",
-  "yield", "cocreate", "coresume", "costatus",
-  "stop", "menuitem", "extcmd", "serial",
-  "_init", "_update", "_update60", "_draw",
-}
-
 return {
   "neovim/nvim-lspconfig",
   opts = {
@@ -28,16 +6,19 @@ return {
         filetypes = { "lua" },
       },
       lua_ls = {
-        on_init = function(client)
-          local root = client.workspace_folders and client.workspace_folders[1] and client.workspace_folders[1].name
-          if root and vim.fn.glob(root .. "/*.p8") ~= "" then
-            client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua or {}, {
-              diagnostics = {
-                disable = { "lowercase-global" },
-                globals = pico8_globals,
-              },
-            })
+        -- Don't attach lua_ls in any project that contains a *.p8 cart.
+        -- lua_ls parses standard Lua grammar, which has no += -= *= /= %=
+        -- or !=/! -- PICO-8's extended operators -- so it flags them as
+        -- syntax errors. pico8_ls (above) understands them and already
+        -- knows the full PICO-8 API, so it's the sole server for those
+        -- buffers; lua_ls still works as normal everywhere else.
+        root_dir = function(bufnr, on_dir)
+          local util = require("lspconfig.util")
+          local fname = vim.api.nvim_buf_get_name(bufnr)
+          if util.root_pattern("*.p8")(fname) then
+            return
           end
+          on_dir(util.root_pattern(".luarc.json", ".luarc.jsonc", ".git")(fname) or vim.fn.getcwd())
         end,
       },
     },
